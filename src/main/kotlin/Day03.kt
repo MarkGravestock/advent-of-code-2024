b464932409ef40634ca70ef326d7ca790e70b9fd
MarkGravestock <mark.gravesotck@gmail.com>
2024-12-03 22:40:13 +0000

    Add Day 3 Part 2

--- src/main/kotlin/Day03.kt
+++ src/main/kotlin/Day03.kt
@@ -7,10 +7,60 @@ class Multiplicator(val testInput: List<String>) {
     }
 
     fun multiplicationArguments(): List<Pair<Int, Int>> {
-        return validInstructions().map { it.removePrefix("mul(").removeSuffix(")").split(",") }.map { Pair(it[0].toInt(), it[1].toInt()) }
+        return validInstructions().map { it.removePrefix("mul(").removeSuffix(")").split(",") }
+            .map { Pair(it[0].toInt(), it[1].toInt()) }
     }
 
     fun totalValue(): Int {
         return multiplicationArguments().sumOf { it.first * it.second }
+    }
+}
+
+
+class AdvancedMultiplicator(val testInput: List<String>) {
+
+    open class Instruction
+    class Do : Instruction()
+    class Dont : Instruction()
+
+    data class Multiply(val arg1: Int, val arg2: Int) : Instruction() {
+        fun value(): Int { return arg1 * arg2 }
+    }
+
+    val mulInstruction = "(mul\\(\\d{1,3},\\d{1,3}\\))|(do\\(\\))|(don't\\(\\))".toRegex()
+
+    fun validInstructions(): List<String> {
+        return testInput.flatMap { mulInstruction.findAll(it) }.map { it.value }.toList()
+    }
+
+    fun multiplicationArguments(): List<Instruction> {
+        return validInstructions().map {
+            when (it.substring(0..2)) {
+                "do(" -> Do()
+                "don" -> Dont()
+                "mul" -> createMultiply(it)
+                else -> throw IllegalArgumentException("Invalid multiplication input: ${it}")
+            }
+        }
+    }
+
+    private fun createMultiply(instruction: String): Multiply {
+        val args = instruction.removePrefix("mul(").removeSuffix(")").split(",")
+        return Multiply(args[0].toInt(), args[1].toInt())
+    }
+
+    fun totalValue(): Int {
+        var total = 0
+        var enabled = true
+
+        for (instruction in multiplicationArguments()) {
+            when (instruction) {
+                is Do -> { enabled = true }
+                is Dont -> { enabled = false }
+                is Multiply -> { if (enabled) total+= instruction.value() }
+            }
+        }
+
+        return total
     }
 }
--- src/test/kotlin/Day03Test.kt
+++ src/test/kotlin/Day03Test.kt
@@ -1,3 +1,4 @@
+import AdvancedMultiplicator.*
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.equals.shouldBeEqual
@@ -46,3 +47,33 @@ class Day03Part1Test: FunSpec ({
 })
 
 
+class Day03Part2Test: FunSpec ({
+
+    context("Part 2 Solution test data") {
+        val testInput =
+            listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+
+        val sut = AdvancedMultiplicator(testInput)
+
+        test("it can match all instructions") {
+            sut.validInstructions() shouldHaveSize 6
+        }
+
+        xtest("it can match multiplication instructions") {
+            sut.multiplicationArguments() shouldBeEqual listOf(
+                Multiply(2,4), Dont(), Multiply(5,5), Multiply(11,8), Do(), Multiply(8,5))
+        }
+
+        test("it should multiply and total") {
+            sut.totalValue() shouldBeEqual 48
+        }
+    }
+
+    test("it calculates the total") {
+        val testInput = readInputForDay(3)
+
+        val sut = AdvancedMultiplicator(testInput)
+
+        sut.totalValue() shouldBeEqual 88802350
+    }
+})
