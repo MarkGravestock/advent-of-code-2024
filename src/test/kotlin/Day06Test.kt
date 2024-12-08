ff2de9bc968dc5d66be9c623f35fb2c7fe4c5c67
MarkGravestock <mark.gravesotck@gmail.com>
2024-12-08 10:08:44 +0000

    Day 6 Part 2

--- src/test/kotlin/Day06Test.kt
+++ src/test/kotlin/Day06Test.kt
@@ -1,4 +1,6 @@
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
 import io.kotest.matchers.shouldBe
 
 class Day06Part1Test : FunSpec({
@@ -19,8 +21,8 @@ class Day06Part1Test : FunSpec({
 
         test("it can move to the second obstruction") {
             val sut = GuardMap(testInput)
-            sut.findNextObstruction()
-            sut.findNextObstruction()
+            sut.findNextObstruction() shouldBe true
+            sut.findNextObstruction() shouldBe true
             sut.current() shouldBe State(Coordinate(8, 1), Directions.South)
         }
 
@@ -43,6 +45,55 @@ class Day06Part1Test : FunSpec({
             sut.totalMoves() shouldBe 5067
         }
     }
+})
+
+class Day06Part2Test : FunSpec({
+
+    context("Part 2 Solution test data") {
+        val testInput = readTestInputForDay(6)
+
+        test("it can read the map and find the exit") {
+            val sut = GuardMap(testInput)
+            sut.addObstruction(Coordinate(0, 0))
+
+            sut.findExit() shouldBe true
+            sut.totalMoves() shouldBe 41
+        }
+
+        test("it exits with obstruction") {
+            forAll(
+                row(Coordinate(3, 6), false),
+                row(Coordinate(6, 7), false),
+                row(Coordinate(7, 7), false),
+                row(Coordinate(1, 8), false),
+                row(Coordinate(3, 8), false),
+                row(Coordinate(7, 9), false),
+                row(Coordinate(0, 0), true),
+
+                ) { obstruction, result ->
+                val sut = GuardMap(testInput)
+                sut.addObstruction(obstruction)
+                sut.findExit() shouldBe result
+            }
+        }
+
+        test("it can calculate how many obstructions cause loops") {
+            val sut = GuardMap(testInput)
+            sut.countAllLoops() shouldBe 6
+        }
+
+    }
+    context("Part 2 Solution real data") {
+        val testInput = readInputForDay(6)
+
+        test("it can calculate how many obstructions cause loops") {
+            val sut = GuardMap(testInput)
+            sut.findExit()
+
+            sut.countAllLoops() shouldBe 1793
+        }
+    }
+
 })
 
 data class State(var coordinate: Coordinate, var direction: Directions) {
@@ -95,6 +146,14 @@ class GuardMap(val input: List<String>) {
 
     private val visited = mutableSetOf(start)
 
+    private val stateAtObstruction = mutableListOf<State>()
+
+    private var obstruction: Coordinate? = null
+
+    fun bounds(): Bounds {
+        return Bounds(IntRange(0, input.size - 1), IntRange(0, input.first().length - 1))
+    }
+
     private fun inBounds(coordinate: Coordinate): Boolean {
         return coordinate.y >= 0 && coordinate.y < input.size && coordinate.x < input.first().length && coordinate.x >= 0
     }
@@ -104,6 +163,7 @@ class GuardMap(val input: List<String>) {
     }
 
     fun at(coordinate: Coordinate) : Char {
+        if (coordinate == obstruction) return '#'
         val char = if (inBounds(coordinate)) input[coordinate.y][coordinate.x] else ' '
         return if(char == '^') '.' else char
     }
@@ -116,21 +176,58 @@ class GuardMap(val input: List<String>) {
         return state
     }
 
-    fun findExit() {
+    fun findExit() : Boolean {
         while (state.direction != Directions.Exit) {
-            findNextObstruction()
-            println(state)
+            if (!findNextObstruction()) {
+                return false
+            }
         }
+
+        return true
     }
 
-    fun findNextObstruction() {
+    fun findNextObstruction() : Boolean {
 
         while(at(state.nextMove()) == '.') {
             state.move()
             visited.add(state.coordinate)
         }
 
+        if (stateAtObstruction.contains(state)) {
+            println("Obstruction ${state}")
+            return false
+        }
+
+        stateAtObstruction.add(State(state.coordinate, state.direction ))
+
         if (at(state.nextMove()) == ' ') state.exit() else state.turn()
+
+        return true
+    }
+
+    fun addObstruction(coordinate: Coordinate) {
+        obstruction = coordinate
+    }
+
+    fun reset() {
+        obstruction = null
+        stateAtObstruction.clear()
+        visited.clear()
+        state = start()
+    }
+
+    fun countAllLoops(): Int {
+        var total = 0
+        for (y in bounds().height) {
+            for (x in bounds().width) {
+                if (at(Coordinate(x, y)) == '.') {
+                    reset()
+                    addObstruction(Coordinate(x, y))
+                    total += if (!findExit()) 1 else 0
+                }
+            }
+        }
+        return total
     }
 }
 
